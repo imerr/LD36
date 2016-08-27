c1ea24a3e25e714abbf1d37f29e6f4d9cace6496
Robin Rolf <imer@imer.cc>
2016-08-27 02:52:22 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "LD36.hpp"
+#include "Player.hpp"
+#include "Rune.hpp"
+#include "Goal.hpp"
+#include "Tip.hpp"
+#include "MagicLight.hpp"
+#include "OneWayPlatform.hpp"
+#include "Switch.hpp"
+#include <Engine/Factory.hpp>
+
+int main() {
+	LD36 game;
+	game.run();
+	return 0;
+}
